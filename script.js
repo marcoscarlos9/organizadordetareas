b655f737cee9b49d8c242131e3803b3fa2e2d7d7
marcoscarlos9 <marcoscarlosaviles@gmail.com>
2023-03-16 16:53:47 -0500

    clase04 completada

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector("[data-form-btn]");
+( () => {const btn = document.querySelector("[data-form-btn]");
 const createTask =("click", (evento) => {
     evento.preventDefault()
     const input = document.querySelector("[data-form-input]");
@@ -7,12 +7,18 @@ const createTask =("click", (evento) => {
     const task = document.createElement("li")
     task.classList.add("card");
     input.value = "";
-    const content = `<div>
-            <i class="far fa-check-square icon"></i>
-            <span class="task">${value}</span>
-        </div>
-        <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+    //backlist
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    taskContent.appendChild(deleteTask());
+    console.log(checkComplete())
+    const content = `<div>`
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
 
     list.appendChild(task)
     console.log(content);
@@ -22,3 +28,27 @@ const createTask =("click", (evento) => {
 console.log(btn);
 //Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask)
+
+const deleteTask = () =>{
+    const i = document.createElement("i");
+    i.classList.add("far");
+    i.classList.add("fa-trash-alt");
+    i.classList.add("trashIcon");
+    i.classList.add("icon");
+    return i;
+}
+
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+}
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon")
+    element.classList.toggle("far");
+}
+})();
